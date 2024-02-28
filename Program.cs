b42a76efdf921097b93c5c59a47e2acda73a9bb5
Karan <himakaran@usf.edu>
2024-02-28 21:54:56 +0530

    Added Comments

--- Program.cs
+++ Program.cs
@@ -1,35 +1,59 @@
-﻿namespace SortApp
+﻿/*
+Author: Nikhil Alla
+Date: 02/28/2024
+Comments: This C# Console Application code demonstrates the
+    Selection Sort Algorithm. No input
+    is required for the script. It will execute without 
+    input against one pre-populated array. 
+*/
+
+namespace SortApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = { 5, 72, 3, 14, 7, 1 };
-            BubbleSort(arr);
-            Console.WriteLine("Sorted array:");
-            foreach (int value in arr)
+            //Console.WriteLine("Hello, World!");
+            int[] selection_sort_numbers = { 5, 72, 3, 14, 7, 1 };
+            Console.WriteLine("These are the Numbers before Sorting.!");
+            foreach (int i in selection_sort_numbers)
             {
-                Console.Write(value + " ");
+                Console.Write(i);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+            int[] sorted_arr = Selection_Sort(selection_sort_numbers);
+            Console.WriteLine("These are the Numbers after Sorting.!");
+            foreach (int i in sorted_arr)
+            {
+                Console.Write(i);
+                Console.Write(' ');
             }
 
         }
 
-        static void BubbleSort(int[] arr)
+        static int[] Selection_Sort(int[] arr)
         {
             int n = arr.Length;
+            int[] sortedArr = new int[n];
+            Array.Copy(arr, sortedArr, n);
+
             for (int i = 0; i < n - 1; i++)
             {
-                for (int j = 0; j < n - i - 1; j++)
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
                 {
-                    if (arr[j] > arr[j + 1])
+                    if (sortedArr[j] < sortedArr[minIndex])
                     {
-
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
+                        minIndex = j;
                     }
                 }
+                int temp = sortedArr[minIndex];
+                sortedArr[minIndex] = sortedArr[i];
+                sortedArr[i] = temp;
             }
+
+            return sortedArr;
         }
     }
 }
